9abca6be4f1146430d0d0dfa4d1c2d8bebb2027f
franciscosucho <110209358+franciscosucho@users.noreply.github.com>
2023-02-23 09:12:47 -0300

    initial commit

---
+++ js/js.js
@@ -0,0 +1,10 @@
+var check = document.querySelector(".check");
+check.addEventListener('click',idioma);
+function idioma(){
+    let id =check.Checked
+    if(id==true){
+        location.href="es/index.html";
+    }else{
+        location.href="../index.html";
+    }
+}
